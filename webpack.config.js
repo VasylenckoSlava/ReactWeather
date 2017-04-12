16ac1584af4c5daa0867f49006197bcca4df5496
Slava Vasylencko <slavavasylencko@gmail.com>
2017-04-12 12:50:51 +0300

    Add error modal

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ WeatherForm: 'app/components/WeatherForm.jsx',
 WeatherMessage: 'app/components/WeatherMessage.jsx',
 About: 'app/components/About.jsx',
 Examples: 'app/components/Examples.jsx',
-openWeatherMap: 'app/api/openWeatherMap.jsx'
+openWeatherMap: 'app/api/openWeatherMap.jsx',
+ErrorModal: 'app/components/ErrorModal.jsx'
     },
     extensions: ['','.js','.jsx']
   },
