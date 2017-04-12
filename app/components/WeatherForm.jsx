7c71b73d99e59f82cd5c777f3a151e62fc7d3236
Slava Vasylencko <slavavasylencko@gmail.com>
2017-04-12 13:18:20 +0300

    Add style css

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -6,7 +6,7 @@ e.preventDefault();
 
 var location = this.refs.location.value;
 
-if(location.length > 0){
+if (location.length > 0) {
   this.refs.location.value = '';
   this.props.onSearch(location);
   }
@@ -16,7 +16,7 @@ if(location.length > 0){
     return (
       <div>
         <form onSubmit={this.onFormSubmit}>
-        <input type="text" ref="location"/>
+        <input type="search" ref="location" placeholder="Search weather by city"/>
         <button className="button expanded hollow">Get Weather</button>
         </form>
 
